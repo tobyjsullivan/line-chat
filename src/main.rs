8cc9234e0a086d3bc82426cc7f21cd8cc45cc3f4
Toby Sullivan <toby.s@canva.com>
2018-03-07 20:50:34 +1100

    Almost send init msg...

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[macro_use]
 extern crate futures;
 extern crate tokio;
+extern crate nix;
 
 use std::{env, io};
 use std::net::SocketAddr;
+use std::os::unix::io::AsRawFd;
+use futures::future::{lazy, empty};
 
 use tokio::prelude::*;
 use tokio::executor::current_thread;
 use tokio::net::UdpSocket;
+
+use nix::sys::socket;
+use nix::sys::socket::sockopt::Broadcast;
 
 struct Server {
     socket: UdpSocket,
@@ -24,24 +30,41 @@ impl Future for Server {
             let (size, peer) = try_ready!(self.socket.poll_recv_from(&mut self.buf));
 
             let amt = try_ready!(self.socket.poll_send_to(&self.buf[..size], &peer));
+            // let bc_addr = "255.255.255.255:1234".parse().unwrap();
+            // let amt = try_ready!(self.socket.poll_send_to(&self.buf[..size], &bc_addr));
             let payload = String::from_utf8(self.buf[..size].to_vec());
             println!("Echoed {}/{} bytes to {} ({:?})", amt, size, peer, payload);
-            self.to_send = None;
         }
     }
 }
 
 fn main() {
     let addr = "0.0.0.0:1234".parse().unwrap();
-    let socket = UdpSocket::bind(&addr).unwrap();
-    println!("Listening on: {}", socket.local_addr().unwrap());
+    let socket1 = UdpSocket::bind(&addr).unwrap();
+    println!("Listening on: {}", socket1.local_addr().unwrap());
 
-
+    socket1.set_broadcast(true).expect("error setting broadcast");
+    // socket::setsockopt(socket1.as_raw_fd(), Broadcast, &true).expect("setsockopt failed");
+ 
     let mut server = Server {
-        socket: socket,
+        socket: socket1,
         buf: vec![0; 1024],
         to_send: None,
     };
+
+    current_thread::block_on_all(lazy(|| {
+        let msg = String::from("init");
+        let bc_addr = "255.255.255.255:1234".parse().unwrap();
+        match server.socket.poll_send_to(&msg.into_bytes().as_slice(), &bc_addr) {
+            Err(e) => return Err(e),
+            _ => println!("init sent")
+        }
+        
+        
+        println!("Sent init!");
+
+        Ok(Async::Ready(()))
+    })).expect("Error on init");
 
     tokio::run(server.map_err(|e| println!("server error = {:?}", e)));
     
